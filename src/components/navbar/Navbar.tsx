7385cfd4f29fa343775fe3d77f0181846ac9fadc
Sang <ptsang1093@gmail.com>
2023-10-09 10:46:01 +0700

    styling for navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,9 +1,31 @@
-import './navbar.scss'
+import './navbar.scss';
 
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
-  )
-}
+    <div className="navbar">
+      <div className="logo">
+        <img src="logo.svg" alt="logo" />
+        <span>Admin Board</span>
+      </div>
+      <div className="icons">
+        <img src="/search.svg" alt="" className="icon" />
+        <img src="/app.svg" alt="" className="icon" />
+        <img src="/expand.svg" alt="" className="icon" />
+        <div className="notification">
+          <img src="/notifications.svg" alt="" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img
+            src="https://cdn-icons-png.flaticon.com/256/616/616554.png"
+            alt=""
+          />
+          <span>Joan</span>
+        </div>
+        <img src="/settings.svg" alt="" className="icon" />
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
