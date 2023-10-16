1e7e515633f4168e6bc5b830474e876862c1f3c4
Sang <ptsang1093@gmail.com>
2023-10-16 21:07:47 +0700

    add data grid from mui

---
+++ src/components/dataTable/DataTable.tsx
@@ -0,0 +1,64 @@
+import { DataGrid, GridColDef, GridToolbar } from '@mui/x-data-grid';
+import './dataTable.scss';
+import { Link } from 'react-router-dom';
+
+type Props = {
+  columns: GridColDef[];
+  rows: object[];
+  slug: string;
+};
+
+const DataTable = (props: Props) => {
+  const handleDelete = (id: number) => {
+    console.log(id);
+  };
+  const columnActions: GridColDef = {
+    field: 'actions',
+    headerName: 'Actions',
+    width: 200,
+    renderCell: (params) => {
+      return (
+        <div className="actions">
+          <Link to={`/${props.slug}/${params.row.id}`}>
+            <img src="./view.svg" alt="" />
+          </Link>
+          <div className="delete" onClick={() => handleDelete(params.row.id)}>
+            <img src="./delete.svg" alt="" />
+          </div>
+        </div>
+      );
+    }
+  };
+
+  return (
+    <div className="dataTable">
+      <DataGrid
+        className="dataGrid"
+        rows={props.rows}
+        columns={[...props.columns, columnActions]}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 8
+            }
+          }
+        }}
+        pageSizeOptions={[5]}
+        checkboxSelection
+        disableRowSelectionOnClick
+        disableColumnFilter
+        disableColumnSelector
+        disableDensitySelector
+        slots={{ toolbar: GridToolbar }}
+        slotProps={{
+          toolbar: {
+            showQuickFilter: true,
+            quickFilterProps: { debounceMs: 500 }
+          }
+        }}
+      />
+    </div>
+  );
+};
+
+export default DataTable;
--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,9 +1,59 @@
-import "./users.scss"
+import { GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import DataTable from '../../components/dataTable/DataTable';
+import { userRows } from '../../data';
+import './users.scss';
+
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'ID', width: 90 },
+  {
+    field: 'avatar',
+    headerName: 'Profile Pic',
+    width: 100,
+    renderCell: (params) => {
+      return <img src={params.row.img || './noavatar.png'} alt="" />;
+    }
+  },
+  {
+    field: 'firstName',
+    headerName: 'First name',
+    width: 150,
+    editable: true
+  },
+  {
+    field: 'lastName',
+    headerName: 'Last name',
+    width: 150,
+    editable: true
+  },
+  {
+    field: 'age',
+    headerName: 'Age',
+    type: 'number',
+    width: 110,
+    editable: true
+  },
+  {
+    field: 'fullName',
+    headerName: 'Full name',
+    description: 'This column has a value getter and is not sortable.',
+    sortable: false,
+    width: 160,
+    valueGetter: (params: GridValueGetterParams) =>
+      `${params.row.firstName || ''} ${params.row.lastName || ''}`
+  },
+  { field: 'status', headerName: 'Status', width: 100, type: 'boolean' }
+];
 
 const Users = () => {
   return (
-    <div className='users'>Users</div>
-  )
-}
+    <div className="users">
+      <div className="info">
+        <h1>Users</h1>
+        <button>Add New User</button>
+      </div>
+      <DataTable slug="users" columns={columns} rows={userRows} />
+    </div>
+  );
+};
 
-export default Users
+export default Users;
